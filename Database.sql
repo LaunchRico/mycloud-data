10d41541dd98adc0675186b6daeceb64317083e0
xmrico <xmrico@teichert.com>
2023-12-21 20:49:01 -0600

    Database sql seed

--- Database.sql
+++ Database.sql
@@ -1,26 +1,44 @@
 CREATE TABLE Users (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
-    username VARCHAR(20) UNIQUE NOT NULL,
-    PRIMARY KEY (id)
+    username VARCHAR(20) UNIQUE NOT NULL
 );
 
 CREATE TABLE Documents (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     user_id INTEGER,
-    PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE DocumentVersions (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     document_id INTEGER,
     modification_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     route VARCHAR(100),
-    PRIMARY KEY (id),
     FOREIGN KEY (document_id) REFERENCES Documents(id) ON DELETE CASCADE
 );
+
+-- Seed script for Users table
+INSERT INTO Users (name, email, password, username)
+VALUES
+    ('John Doe', 'john@example.com', 'password123', 'johndoe'),
+    ('Alice Smith', 'alice@example.com', 'pass456', 'alicesmith'),
+    ('Bob Johnson', 'bob@example.com', 'bobjohnsonpass', 'bobby');
+
+-- Seed script for Documents table
+INSERT INTO Documents (name, user_id)
+VALUES
+    ('Document 1', 1),
+    ('Document 2', 2),
+    ('Document 3', 1);
+
+-- Seed script for DocumentVersions table
+INSERT INTO DocumentVersions (document_id, route)
+VALUES
+    (1, '/path/to/document1'),
+    (2, '/path/to/document2'),
+    (3, '/path/to/document3');
